bcd29a2b64967a1771f9a011c7b5cbb5bc921e58
Matt Dziuban <mrdziuban@gmail.com>
2017-02-27 19:46:29 -0500

    Add simple loader to shell out to dart2js.

---
+++ index.js
@@ -0,0 +1,19 @@
+const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function(source) {
+  const callback = this.async();
+  const tmp = path.join(__dirname, 'tmp');
+  const fname = path.basename(this.resourcePath, '.dart');
+  const cmd = `dart2js -o '${path.join(tmp, `${fname}.js`)}' '${this.resourcePath}'`;
+  child_process.execSync(`rm -rf '${tmp}'`);
+  child_process.execSync(`mkdir -p '${tmp}'`);
+
+  child_process.exec(cmd, function(error, stdout, stderr) {
+    if (error) { return callback(error, null); }
+    const out = fs.readFileSync(path.join(tmp, `${fname}.js`), 'utf8');
+    callback(null, out);
+  });
+};
+
