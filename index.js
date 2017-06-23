d12678b1549ecac6d2b7b873d54c85448a9cefbf
Dmitry Guzeev <dmitry.guzeev@yahoo.com>
2017-06-23 16:36:57 +0300

    Add loader options
    This commit adds the options to the loader.
    The new options available are:
     - minimze
     - typeCheck
    
    Those are converted to the `dart2js` utility
    command-line options directly.
    
    The `minimize` option corresponds to the `-m`
    flag, whereas `typeCheck` - to `-c`.

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const colors = require('colors');
+const loaderUtils = require('loader-utils');
+
+/**
+ * @summary
+ * The map between the options passed to the loader
+ * and the command-line `dart2js` utility flags.
+ */
+const optsMap = {
+  'minimize': '-m',
+  'typeCheck': '-c',
+};
+
+/**
+ * @summary
+ * Construct a command-line `dart2js` utility options string
+ * from a given loader options object.
+ *
+ * @return {string}
+ */
+const constructDart2JsOptions = (options) =>
+  Object.keys(options).reduce((res, key) => {
+    const mapping = optsMap[key];
+    return mapping ? `${res} ${mapping}` : res;
+  }, '');
 
 module.exports = function(source) {
+  // Retrieve the options that were passed to the loader
+  const options = loaderUtils.getOptions(this);
+
+  // Construct the command-line options to the `dart2js` utility
+  const dart2JsOptionsStr = constructDart2JsOptions(options);
+
   const callback = this.async();
   const tmp = path.join(__dirname, 'tmp');
   const fname = path.basename(this.resourcePath, '.dart');
-  const cmd = `dart2js -o '${path.join(tmp, `${fname}.js`)}' '${this.resourcePath}'`;
+  const cmd = `dart2js ${dart2JsOptionsStr} -o '${path.join(tmp, `${fname}.js`)}' '${this.resourcePath}'`;
   child_process.execSync(`rm -rf '${tmp}'`);
   child_process.execSync(`mkdir -p '${tmp}'`);
 
